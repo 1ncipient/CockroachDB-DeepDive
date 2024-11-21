a58e198bf9bee03e38303aeaf31ba7847bb68bef
JZhang0 <57733075+JZhang0@users.noreply.github.com>
2024-11-20 20:04:08 -0500

    unifying movie id across tables

--- MovieLens/data_process.py
+++ MovieLens/data_process.py
@@ -1,7 +1,48 @@
 #!/usr/bin/env python3
 
+import gc
 import argparse
 import pandas as pd
+
+def unify_movieId(data_path):
+    movies_metadata = pd.read_csv(data_path + 'movies_metadata.csv')
+    credits = pd.read_csv(data_path + 'credits.csv')
+    links = pd.read_csv(data_path + 'links.csv')
+
+    # movies_metadata.columns = [x.replace("\r", "") for x in movies_metadata.columns.to_list()]
+
+    movies_metadata.drop_duplicates(subset='id', keep='first', inplace=True)
+    movies_metadata.drop_duplicates(subset='imdb_id', keep='first', inplace=True)
+    credits.drop_duplicates(subset='id', keep='first', inplace=True)
+    links.drop_duplicates(subset='tmdbId', keep='first', inplace=True)
+    movies_metadata['imdbId'] = movies_metadata['imdb_id'].str.lstrip('tt').astype('float')
+
+    tmdb_to_movieId_map = dict(zip(links['tmdbId'], links['movieId']))
+    movies_metadata['movieId'] = movies_metadata['id'].map(tmdb_to_movieId_map)
+    credits['movieId'] = credits['id'].map(tmdb_to_movieId_map)
+
+    valid_movieIds = set(links['movieId'].dropna()) & \
+                    set(credits['movieId'].dropna()) & \
+                    set(movies_metadata['movieId'].dropna())
+    
+    links = links[links['movieId'].isin(valid_movieIds)]
+    credits = credits[credits['movieId'].isin(valid_movieIds)]
+    movies_metadata = movies_metadata[movies_metadata['movieId'].isin(valid_movieIds)]
+
+    movies = pd.read_csv(data_path + 'movies.csv')
+    ratings = pd.read_csv(data_path + 'ratings.csv')
+    tags = pd.read_csv(data_path + 'tags.csv')
+
+    movies = movies[movies['movieId'].isin(valid_movieIds)]
+    ratings = ratings[ratings['movieId'].isin(valid_movieIds)]
+    tags = tags[tags['movieId'].isin(valid_movieIds)]
+
+    movies_metadata.to_csv(data_path + 'movies_metadata.csv', index=False)
+    credits.to_csv(data_path + 'credits.csv', index=False)
+    links.to_csv(data_path + 'links.csv', index=False)
+    movies.to_csv(data_path + 'movies.csv', index=False)
+    ratings.to_csv(data_path + 'ratings.csv', index=False)
+    tags.to_csv(data_path + 'tags.csv', index=False)
 
 def main():
     # Set up argument parser
@@ -12,21 +53,22 @@ def main():
     # Get the path from the arguments
     data_path = args.path
 
+    unify_movieId(data_path)
+    
     movies_metadata = pd.read_csv(data_path + 'movies_metadata.csv', lineterminator='\n')
     credits = pd.read_csv(data_path + 'credits.csv', lineterminator='\n')
+    
+    # Remove unnecessary columns in movie metadata
+    movies_metadata.drop(columns=['belongs_to_collection', 'genres', 'homepage', 'status', 'original_title', 'video', 'imdb_id', 'id', 'imdbId'], inplace=True, errors='ignore')
 
-    credits.columns = [x.replace("\r", "") for x in credits.columns.to_list()]
-    movies_metadata.columns = [x.replace("\r", "") for x in movies_metadata.columns.to_list()]
-
-    movies_metadata.drop(columns=['belongs_to_collection', 'genres', 'homepage', 'status', 'original_title', 'video'], inplace=True, errors='ignore')
-    movies_metadata.rename(columns={'id' : 'tmdb_id'}, inplace=True)
-
+    # Format the json data
     movies_metadata['production_companies'] = movies_metadata['production_companies'].str.replace("'", '"').str.replace("None", 'null').str.replace("True", 'true').str.replace("False", 'false')
     movies_metadata['production_countries'] = movies_metadata['production_countries'].str.replace("'", '"').str.replace("None", 'null').str.replace("True", 'true').str.replace("False", 'false')
     movies_metadata['spoken_languages'] = movies_metadata['spoken_languages'].str.replace("'", '"').str.replace("None", 'null').str.replace("True", 'true').str.replace("False", 'false')
 
-    credits.rename(columns={'id' : 'tmdb_id'}, inplace=True)
+    movies_metadata.drop(columns='id', inplace=True, errors='ignore')
 
+    # Format the json data
     credits['cast'] = credits['cast'].str.replace("'", '"').str.replace("None", 'null').str.replace("True", 'true').str.replace("False", 'false')
     credits['crew'] = credits['crew'].str.replace("'", '"').str.replace("None", 'null').str.replace("True", 'true').str.replace("False", 'false')
 
--- setup.sh
+++ setup.sh
@@ -20,17 +20,23 @@ fi
 if  [[ $1 = "-r" ]]; then
     echo -e "\n${GREEN_COLOR}Downloading fresh copy of data${NO_COLOR}\n"
 
-    find "$DIR/MovieLens/" -maxdepth 1 ! -name data_processing.ipynb -name data_process.py -type f -delete
+    find "$DIR/MovieLens/" -maxdepth 1 ! -name data_processing.ipynb ! -name data_process.py -type f -delete
 
+    echo -e "\n${GREEN_COLOR}Downloading movie metadata${NO_COLOR}\n"
     curl -L -o "$DIR/MovieLens/movie_metadata.zip" https://www.kaggle.com/api/v1/datasets/download/thanhnghth/movielens-metadata-datasets
+
+    echo -e "\n${GREEN_COLOR}Downloading movie data${NO_COLOR}\n"
     curl -L -o "$DIR/MovieLens/ml-latest.zip" https://files.grouplens.org/datasets/movielens/ml-latest.zip
 
     unzip "$DIR/MovieLens/movie_metadata.zip" -d "$DIR/MovieLens/"
     unzip -j "$DIR/MovieLens/ml-latest.zip" -d "$DIR/MovieLens/"
 
     rm "$DIR/MovieLens/ml-latest.zip" "$DIR/MovieLens/movie_metadata.zip" "$DIR/MovieLens/keywords.csv"
+
+    echo -e "\n${GREEN_COLOR}Movie data download complete${NO_COLOR}\n"
 fi
 
+echo -e "\n${GREEN_COLOR}Processing movie data${NO_COLOR}\n"
 "$DIR/MovieLens/data_process.py" "$DIR/MovieLens/"
 
 exit 0
