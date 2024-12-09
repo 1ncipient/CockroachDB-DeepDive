c08e468337af2c709f0e970691ccbe4d0a6b1b22
JZhang0 <57733075+JZhang0@users.noreply.github.com>
2024-12-09 17:13:34 -0500

    moving scripts together

--- scripts/dataset-setup.sh
+++ scripts/dataset-setup.sh
@@ -3,6 +3,8 @@
 SCRIPT_PATH=$(readlink -f "$0")
 DIR=$(dirname "$SCRIPT_PATH")
 
+MOVIE_LENS_PATH="$DIR/../MovieLens"
+
 GREEN_COLOR='\033[1;32m'
 RED_COLOR='\033[0;31m'
 NO_COLOR='\033[0m'
@@ -22,23 +24,23 @@ fi
 if  [[ $1 = "-r" ]]; then
     echo -e "\n${GREEN_COLOR}Downloading fresh copy of data${NO_COLOR}\n"
 
-    find "$DIR/MovieLens/" -maxdepth 1 ! -name data_processing.ipynb ! -name data_process.py -type f -delete
+    find "$MOVIE_LENS_PATH/" -maxdepth 1 ! -name data_processing.ipynb ! -name data_process.py -type f -delete
 
     echo -e "\n${GREEN_COLOR}Downloading movie metadata${NO_COLOR}\n"
-    curl -L -o "$DIR/MovieLens/movie_metadata.zip" https://www.kaggle.com/api/v1/datasets/download/thanhnghth/movielens-metadata-datasets
+    curl -L -o "$MOVIE_LENS_PATH/movie_metadata.zip" https://www.kaggle.com/api/v1/datasets/download/thanhnghth/movielens-metadata-datasets
 
     echo -e "\n${GREEN_COLOR}Downloading movie data${NO_COLOR}\n"
-    curl -L -o "$DIR/MovieLens/ml-latest.zip" https://files.grouplens.org/datasets/movielens/ml-latest.zip
+    curl -L -o "$MOVIE_LENS_PATH/ml-latest.zip" https://files.grouplens.org/datasets/movielens/ml-latest.zip
 
-    unzip "$DIR/MovieLens/movie_metadata.zip" -d "$DIR/MovieLens/"
-    unzip -j "$DIR/MovieLens/ml-latest.zip" -d "$DIR/MovieLens/"
+    unzip "$MOVIE_LENS_PATH/movie_metadata.zip" -d "$MOVIE_LENS_PATH/"
+    unzip -j "$MOVIE_LENS_PATH/ml-latest.zip" -d "$MOVIE_LENS_PATH/"
 
-    rm "$DIR/MovieLens/ml-latest.zip" "$DIR/MovieLens/movie_metadata.zip" "$DIR/MovieLens/keywords.csv" "$DIR/MovieLens/tags.csv"
+    rm "$MOVIE_LENS_PATH/ml-latest.zip" "$MOVIE_LENS_PATH/movie_metadata.zip" "$MOVIE_LENS_PATH/keywords.csv" "$MOVIE_LENS_PATH/tags.csv"
 
     echo -e "\n${GREEN_COLOR}Movie data download complete${NO_COLOR}\n"
 fi
 
 echo -e "\n${GREEN_COLOR}Processing movie data${NO_COLOR}\n"
-"$DIR/MovieLens/data_process.py" "$DIR/MovieLens/"
+"$MOVIE_LENS_PATH/data_process.py" "$MOVIE_LENS_PATH/"
 
 exit 0
